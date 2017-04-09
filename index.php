46edb063097bf5eb00504d8b31a31a1c0b119867
Judes Steinar <jsteinar7@gmail.com>
2017-04-09 18:59:02 +0530

    index page of our website

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Speak English</title>
+    </head>
+    <body>
+        <h1>Welcome..!</h1>
+        <a href="login.php">Login</a><br>
+        <a href="register.php">Register</a>
+    </body>
+</html>
