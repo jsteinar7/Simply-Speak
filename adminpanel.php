6fde8cf05d80e3d74061f286cea40f50373808d6
Judes Steinar <jsteinar7@gmail.com>
2017-04-25 22:01:30 +0530

    Add files via upload

--- adminpanel.php
+++ adminpanel.php
@@ -1,25 +1,38 @@
 <?php
- 
  session_start();
  
   // if session is not set this will redirect to login page
- if(!isset($_SESSION['admin']))
- {
+ if(!isset($_SESSION['admin'])){
   header("Location: adminlogin.php");
   exit;
  }
 ?>
-
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>Admin Panel</title>
-    </head>
-    <body>
-        <h1>Welcome Admin</h1>
-        <form action="adminpanel.php" method="post">
-            <input type="file" name="file">
-            <input type="submit" name="sub">
-        </form>
-        <br><a href="adminlogout.php">Logout</a>
-    </body>
+<head>
+	<title>Adosat Upload the questions</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+    <a href="adminlogout.php"><br><button type="button" class="btn btn-danger pull-right">Logout</button></a>
+	<h1>Bulk Upload </h1>
+
+	<form method="POST" action="excelUpload.php" enctype="multipart/form-data">
+		<div class="form-group">
+			<label>Upload the Excel File which has the questions</label>
+			<input type="file" name="file" class="form-control">
+		</div>
+		<div class="form-group">
+			<button type="submit" name="Submit" class="btn btn-success">Upload</button>
+                        
+         
+                </div>
+	</form>
+        <hr><div class="btn">
+            <h3 class="alert-link">View the <a href="#"><strong>scores of the candidates</strong></a></h3></div>
+</div>
+
+</body>
 </html>
