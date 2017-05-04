c56df31819a76ea816d0a7b9b5caf44058adf892
Judes Steinar <jsteinar7@gmail.com>
2017-05-04 21:34:25 +0530

    Minor changes

--- index.php
+++ index.php
@@ -1,92 +1,88 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>ADOSAT</title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
-		<script src="js/jquery.min.js"></script>
+    <head>
+        <meta charset="UTF-8">
+        <title>ADOSAT</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+        <script src="js/jquery.min.js"></script>
         <script src="js/wow.js"></script>
         <script>
             new WOW().init();
         </script>
         <style>
             .btn-space {
-    margin-right: 45px;
-}
+                margin-right: 45px;
+            }
         </style>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-layers.min.js"></script>
-		<script src="js/init.js"></script>
-		<noscript>
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-xlarge.css" />
-           
-		</noscript>
-		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
-         <link rel="stylesheet" href="css/animate.css">
-	</head>
-	<body class="landing">
+        <script src="js/skel.min.js"></script>
+        <script src="js/skel-layers.min.js"></script>
+        <script src="js/init.js"></script>
+        <noscript>
+            <link rel="stylesheet" href="css/skel.css" />
+            <link rel="stylesheet" href="css/style.css" />
+            <link rel="stylesheet" href="css/style-xlarge.css" />
+        </noscript>
+        <!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+        <link rel="stylesheet" href="css/animate.css">
+    </head>
+
+    <body class="landing">
 
         <!-- Header -->
         <header id="header">
                 <h1><a href="index.php">ADOSAT</a></h1>
-                
         </header>
 
-    <!-- Banner -->
-            <section id="banner" >
-    <div class="animated fadeInDown">
-    <h3>This is</h3>
-                    <h2 >ADOSAT</h2>
-                    <p>Center for Excellence </p>
-                    <ul class="actions">
-                            <li>
-                                <a href="login.php" class="btn-space button big">LOGIN</a>
-                                    <a href="register.php" class="btn-space button big">REGISTER</a>
-                                    <a href="adminlogin.php" class="btn-space button big">ADMIN</a>
-                            </li>
-                    </ul>
-    </div>
-            </section>
+        <!-- Banner -->
+        <section id="banner" >
+            <div class="animated fadeInDown">
+              <h3>This is</h3>
+              <h2 >ADOSAT</h2>
+              <p>Center for Excellence </p>
+              <ul class="actions">
+                  <li>
+                      <a href="login.php" class="btn-space button big">LOGIN</a>
+                      <a href="register.php" class="btn-space button big">REGISTER</a>
+                      <a href="adminlogin.php" class="btn-space button big">ADMIN</a>
+                  </li>
+              </ul>
+            </div>
+        </section>
 
-    <!-- One -->
-            <section id="one" class="wrapper style1 align-center">
-                    <div class="container ">
-                            <header>
-                                    <h2>Master Your Skills</h2>
-                            </header>
-                            <div class="row 200% ">
-                                    <section class="4u 12u(small) wow animated zoomIn">
-                                            <i class="icon big rounded fa-clock-o"></i>
-                                            <p>Test Your English Efficiency</p>
-                                    </section>
-                                    <section class="4u 12u(small) wow animated zoomIn">
-                                            <i class="icon big rounded fa-comments"></i>
-                                            <p>Develop Your Communication Skills</p>
-                                    </section>
-                                    <section class="4u 12u(small) wow animated zoomIn">
-                                            <i class="icon big rounded fa-user"></i>
-                                            <p>Be A Professional</p>
-                                    </section>
-                            </div>
-                    </div>
-            </section>
+        <!-- One -->
+        <section id="one" class="wrapper style1 align-center">
+            <div class="container ">
+                <header>
+                    <h2>Master Your Skills</h2>
+                </header>
+            <div class="row 200% ">
+                <section class="4u 12u(small) wow animated zoomIn">
+                    <i class="icon big rounded fa-clock-o"></i>
+                    <p>Test Your English Efficiency</p>
+                </section>
+                <section class="4u 12u(small) wow animated zoomIn">
+                    <i class="icon big rounded fa-comments"></i>
+                    <p>Develop Your Communication Skills</p>
+                </section>
+                <section class="4u 12u(small) wow animated zoomIn">
+                    <i class="icon big rounded fa-user"></i>
+                    <p>Be A Professional</p>
+                </section>
+                </div>
+            </div>
+        </section>
 
-
-
-    <!-- Footer -->
-            <footer id="footer">
-                    <div class="container">
-                            <div class="row"></div>
-                    <ul class="copyright">
-                            <li>&copy; COPYRIGHTS. All rights reserved.</li>
-                    </ul>
-                    </div>
-            </footer>
-
-	</body>
+        <!-- Footer -->
+        <footer id="footer">
+            <div class="container">
+                <div class="row"></div>
+            <ul class="copyright">
+                <li>&copy; COPYRIGHTS. All rights reserved.</li>
+            </ul>
+            </div>
+        </footer>
+    </body>
 </html>
